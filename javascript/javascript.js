4e0d1e57e9dbcfa11a862686c51d2d12fa77aa9a
BrandonSaravia <BPSaravia@gmail.com>
2019-03-15 10:39:06 -0400

    finished hover of images

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,7 +4,19 @@ window.addEventListener('scroll', function(e) {
   }
 );
 
+$(function(){
 
+  $(".tilt-card").tilt({
+    maxTilt: 15,
+    perspective: 1400,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    speed: 1200,
+    glare: true,
+    maxGlare: 0.2,
+    scale: 1
+  });
+
+})
 
 
 
